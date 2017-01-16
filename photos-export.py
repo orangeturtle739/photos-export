9e021e331b1b8301f2d0702e3cef1643acb61dac
Jacob Glueck <swimgiraffe435@gmail.com>
2017-01-16 09:54:09 -0500

    Add location support

--- photos-export.py
+++ photos-export.py
@@ -1,6 +1,12 @@
 #! /usr/bin/env python3
 
-import sys, os, sqlite3, hashlib, json
+import sys
+import os
+import sqlite3
+import hashlib
+import json
+import progressbar
+from libxmp import XMPFiles
 
 lib_dir = sys.argv[1]
 output_dir = sys.argv[2]
@@ -15,7 +21,9 @@ for subdir, dirs, files in os.walk(edited_root):
         images = os.listdir(subdir)
         if len(images) != 1:
             print('Error! "%s" should contain 1 image.', images)
-        edited_index[os.path.basename(subdir)] = os.path.join(subdir, images[0])
+        edited_index[
+            os.path.basename(subdir)] = os.path.join(
+            subdir, images[0])
 # print(edited_index)
 # print(main_db_path)
 # print(proxy_db_path)
@@ -25,17 +33,21 @@ main_db.row_factory = sqlite3.Row
 proxy_db = sqlite3.connect(proxy_db_path)
 proxy_db.row_factory = sqlite3.Row
 
-c = main_db.cursor();
+c = main_db.cursor()
+c.execute('SELECT COUNT(*) from RKMaster')
+(number_of_rows,) = c.fetchone()
 c.execute('SELECT * FROM RKMaster')
 uuids = {}
-euuids = {}
-for master in iter(c.fetchone, None):
+bar = progressbar.ProgressBar(max_value=number_of_rows)
+for master in bar(iter(c.fetchone, None)):
     master_uuid = master['uuid']
     master_path = os.path.join(lib_dir, 'Masters', master['imagePath'])
+    latitude = None
+    longitude = None
     master_albums = set([])
     master_keywords = set([])
     master_rating = None
-    vc = main_db.cursor();
+    vc = main_db.cursor()
     vc.execute('SELECT * FROM RKVersion WHERE masterUuid=?', [master_uuid])
     edited_paths = []
     unadjusted_count = 0
@@ -43,39 +55,59 @@ for master in iter(c.fetchone, None):
         edited_path = []
         is_master = False
         if version['adjustmentUuid'] != 'UNADJUSTEDNONRAW':
-            ac = proxy_db.cursor();
-            ac.execute('SELECT * FROM RKModelResource WHERE resourceTag=?', [version['adjustmentUuid']])
+            ac = proxy_db.cursor()
+            ac.execute('SELECT * FROM RKModelResource WHERE resourceTag=?',
+                       [version['adjustmentUuid']])
             for resource in iter(ac.fetchone, None):
-                if resource['attachedModelType'] == 2 and resource['resourceType'] == 4:
+                if resource['attachedModelType'] == 2 and resource[
+                        'resourceType'] == 4:
                     if len(edited_path) != 0:
-                        print("Warning! Multiple valid edits!")
+                        pass
+                        # print("Warning! Multiple valid edits!")
                     edited_path += [edited_index[resource['resourceUuid']]]
         else:
             unadjusted_count += 1
             is_master = True
 
+        new_latitude = version['latitude']
+        new_longitude = version['longitude']
+        if latitude is None or longitude is None:
+            latitude = new_latitude
+            longitude = new_longitude
+        elif abs((new_latitude or 100000) - latitude) > 0.00001 and abs((new_longitude or 100000) - longitude) > 0.00001:
+            print("Inconsistent location: (%f, %f) -> (%f, %f)" %
+                  (latitude, longitude, new_latitude, new_longitude))
+
         kc = main_db.cursor()
-        kc.execute('SELECT * FROM RKAlbumVersion WHERE versionId=?', [version['modelId']])
+        kc.execute('SELECT * FROM RKAlbumVersion WHERE versionId=?',
+                   [version['modelId']])
         albums = set([])
         for album_id in iter(kc.fetchone, None):
             alc = main_db.cursor()
-            alc.execute('SELECT * FROM RKAlbum WHERE modelId=?', [album_id['albumId']])
+            alc.execute('SELECT * FROM RKAlbum WHERE modelId=?',
+                        [album_id['albumId']])
             r_albums = alc.fetchall()
             if len(r_albums) != 0:
                 if len(r_albums) != 1:
-                    print("Warning! More than one album for ID %d" % album_id['albumId'])
+                    print(
+                        "Warning! More than one album for ID %d" %
+                        album_id['albumId'])
                 albums |= set([r_albums[0]['name']])
 
         wc = main_db.cursor()
-        wc.execute('SELECT * FROM RKKeywordForVersion WHERE versionId=?', [version['modelId']])
+        wc.execute(
+            'SELECT * FROM RKKeywordForVersion WHERE versionId=?', [version['modelId']])
         keywords = set([])
         for keyword_id in iter(kc.fetchone, None):
             klc = main_db.cursor()
-            klc.execute('SELECT * FROM RKKeyword WHERE modelId=?', [keyword_id['keywordId']])
+            klc.execute('SELECT * FROM RKKeyword WHERE modelId=?',
+                        [keyword_id['keywordId']])
             r_keyword = klc.fetchall()
             if len(r_keyword) != 0:
                 if len(r_keyword) != 1:
-                    print("Warning! More than one keyword for ID %d" % keyword_id['keywordId'])
+                    print(
+                        "Warning! More than one keyword for ID %d" %
+                        keyword_id['keywordId'])
                 keywords |= set([r_keyword[0]['name']])
 
         rating = version['mainRating']
@@ -85,37 +117,78 @@ for master in iter(c.fetchone, None):
             master_rating = rating
         else:
             for foo in edited_path:
-                euuid = hashlib.md5(open(foo,'rb').read()).hexdigest()
-                if euuid not in euuids:
-                    euuids[euuid] = 0
-                euuids[euuid] += 1
-                edited_paths += [{'path': foo, 'albums': list(albums), 'keywords': list(keywords), 'rating': rating, 'uuid': 'e-%s%03d' % (euuid, euuids[euuid]-1)}]
+                uuid = hashlib.sha256(open(foo, 'rb').read()).hexdigest()
+                if uuid not in uuids:
+                    uuids[uuid] = 0
+                uuids[uuid] += 1
+                edited_paths += [{'path': foo,
+                                  'albums': list(albums),
+                                  'keywords': list(keywords),
+                                  'rating': rating,
+                                  'euuid': '%s-%03d' % (uuid,
+                                                        uuids[uuid] - 1),
+                                  'in_library': True}]
 
     master_in_library = (unadjusted_count != 0)
-    uuid = hashlib.md5(open(master_path,'rb').read()).hexdigest()
+    uuid = hashlib.sha256(open(master_path, 'rb').read()).hexdigest()
     if uuid in uuids:
-        print("Duplicate photo: %s, %s" % (uuid, master_path))
+        # print("Duplicate photo: %s, %s" % (uuid, master_path))
+        pass
     else:
         uuids[uuid] = 0
     uuids[uuid] += 1
-    extended_uuid = '%s%03d' % (uuid, uuids[uuid]-1)
+    extended_uuid = '%s-%03d' % (uuid, uuids[uuid] - 1)
 
-    photo_data = {'extended_uuid': extended_uuid, 'path': master_path, 'master_in_library': master_in_library, 'albums': list(master_albums), 'keywords': list(master_keywords), 'rating': master_rating, 'edits': edited_paths}
-    json_data = json.dumps(photo_data)
-    print(json_data)
+    base_data = {'latitude': latitude, 'longitude': longitude}
+    master_data = {
+        'euuid': extended_uuid,
+        'path': master_path,
+        'in_library': master_in_library,
+        'albums': list(master_albums),
+        'keywords': list(master_keywords),
+        'rating': master_rating}
+    # print(dict(base_data, **master_data))
+    # print(edited_paths)
     if unadjusted_count != 0 and unadjusted_count != 1:
-        print("Warning! %d unadjusted images!" % unadjusted_count)
+        # print("Warning! %d unadjusted images!" % unadjusted_count)
+        print
 
     # Export!
     base_export_path = os.path.join(output_dir, extended_uuid)
     # Hard link the master
-    os.link(master_path, os.path.join(output_dir, extended_uuid + os.path.splitext(master_path)[1]))
+    os.link(
+        master_path,
+        os.path.join(
+            output_dir,
+            extended_uuid +
+            os.path.splitext(master_path)[1].lower()))
     # Write the data
     with open(os.path.join(output_dir, '%s.json' % extended_uuid), 'w') as log_file:
-        print(json_data, file=log_file)
+        print(
+            json.dumps(
+                dict(
+                    dict(
+                        base_data,
+                        **master_data),
+                    derived_from=None)),
+            file=log_file)
     # Hard link the edits
     edit_export_path = os.path.join(base_export_path, 'edited')
     for edit_info in edited_paths:
-            os.link(edit_info['path'], os.path.join(output_dir, '%s-%s%s'% (extended_uuid, edit_info['uuid'], os.path.splitext(edit_info['path'])[1])))
-
-    print("****************")
+        os.link(
+            edit_info['path'],
+            os.path.join(
+                output_dir,
+                '%s%s' %
+                (edit_info['euuid'],
+                 os.path.splitext(
+                    edit_info['path'])[1].lower())))
+        with open(os.path.join(output_dir, '%s.json' % edit_info['euuid']), 'w') as log_file:
+            print(
+                json.dumps(
+                    dict(
+                        dict(
+                            base_data,
+                            **edit_info),
+                        derived_from=extended_uuid)),
+                file=log_file)
