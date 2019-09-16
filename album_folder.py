daa1b82b9c74d959d339f77bf6ff85bc006415b0
Alex <298750+alexantao@users.noreply.github.com>
2019-09-16 14:09:00 -0300

    Add files via upload
    This new version  has a lot of changes. I realized that in the last version, just moving the file didn't work, because if the photo is included in several albuns, the other copies will not be done. So, this new version first copies the photo to all other albuns, and then it moves the file to the last album listed.

--- album_folder.py
+++ album_folder.py
@@ -4,29 +4,52 @@ import json
 import progressbar
 import os
 import sys
-from shutil import copyfile
 from argparse import ArgumentParser
+from shutil import copyfile
+from shutil import move
+
+
+# Source_dir : passed as parameter, where your photos are located
+# output_dir : directory under where all albuns will be created
 
 def run(source_dir, output_dir):
     bar = progressbar.ProgressBar()
-    for f in bar(os.listdir(source_dir)):
-        if os.path.splitext(f)[1] == '.json':
+
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+
+    num_of_missing = 0
+    for f in bar(os.listdir(source_dir)):                                       # f is each file in directory
+        (root_file, ext) = os.path.splitext(f)
+        if ext == '.json':                                                      # only .json are treated
             with open(os.path.join(source_dir, f)) as data_file:
                 data = json.load(data_file)
-                for album in data['albums']:
+                number_of_albums = len(data['albums'])                          # number of albuns, to copy to various albuns
+                album_counter = 1
+                for album in data['albums']:                                    # list of all albuns the photo is included
                     album = album.replace("/", "_")
                     if not os.path.exists(os.path.join(output_dir, album)):
-                        os.mkdir(os.path.join(output_dir, album))
+                        os.mkdir(os.path.join(output_dir, album))               # Create the albun on destination directory
 
-                    imagesource = data['path']
                     imagename = data['path'].split(os.sep)[-1]
+                    imagesource = os.path.join(source_dir, imagename)           # the file must be with the json file, copied from extract_photos.py
                     imagedestination = os.path.join(output_dir, album, imagename)
 
-                    if not args.move:
-                        copyfile(imagesource, imagedestination)
-                    else:
-                        move(imagesource, imagedestination)
 
+                    # temporary commented, until tested it all right, no copies, no moves.
+                    if album_counter == number_of_albums:
+                        if os.path.exists(imagesource):                                    # let's not consider the photo was exported or will raise an exception
+                            #move(imagesource, imagedestination)       # this is the last album, so, move the file
+                            print("Moving : ", imagesource, " -> ", imagedestination)
+                        else:
+                            print("Missing File:", imagesource)
+                            num_of_missing += 1
+                    else:
+                        #copyfile(imagesource, imagedestination)   # temporary commented, until tested it all right, no copies, no moves.
+                        print("\tCopying: ", imagesource, " -> ", imagedestination)
+                        album_counter += 1
+
+    print ("Number of missing files: ", num_of_missing)
 
 # Usage: ./album_folder <source_dir> <output_dir>
 # Copies all files from source_dir to a folder-based map structure in output_dir
@@ -35,9 +58,12 @@ if __name__ == '__main__':
 
     # Options parsed from command line
     parser = ArgumentParser()
-    parser.add_argument("--move", action="store_true", default=False, help='Defines if the file is to move or copy from SourceDir to DestDir')
-    parser.add_argument('source_dir', help='Source from where the photos will be processed')
-    parser.add_argument('output_dir', help='Destination dir of the photos')
-    args = parser.parse_args()
+    parser.add_argument('source_dir', help='Path of where the .json and photos were exported')
+    parser.add_argument('output_dir', help='Path to where the albuns and the files will be created/moved')
+
+    try:
+        args = parser.parse_args()
+    except:
+        sys.exit(2)
 
     run(args.source_dir, args.output_dir)
